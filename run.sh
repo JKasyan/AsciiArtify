c39bc4bcb97e7c352404757f36777e2665691c81
evgenii.kasyan <evgenii.kasyan@maxpay.com>
2024-07-24 14:28:09 +0300

    run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ VERSION=$(git describe --tags --abbrev=0)
 if [ -z $VERSION ];then
   echo "version is empty"
 fi
-TAG=ekasyan/AsciiArtify:$VERSION
-echo $TAG - $VERSION
+TAG=ekasyan/ascii_artify:$VERSION
+echo "tag -- $TAG"
+echo "version -- $VERSION"
 
-docker build -t . ekasyan/AsciiArtify:$VERSION
+docker build -t $TAG .
 docker push $TAG
